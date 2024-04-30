a85ddc4f38f5919fb2c838f3cd1ba36b595226ae
John Dippenaar <231268@virtualwindow.co.za>
2024-04-30 12:59:16 +0200

    Update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+console.log("I am " + 44 + " years old");
+console.log(80+32);
+
+let ticketNum =3;
+const pi=3.14;
+
+let firstName="John", surname= "Dippenaar", faveNum=44;
